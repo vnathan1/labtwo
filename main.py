59ad38c1aa749e33ec99656c42e87728e9da839b
vnathan1 <91503462+vnathan1@users.noreply.github.com>
2021-09-29 00:47:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+''' Pin Setup
+led 1 = GPIO17
+led 2 = GPIO27
+led 3 = GPIO18
+button1 = GPIO22
+button2 = GPIO23 '''
+
+# LED 3 Constant Blinking:
+GPIO.setmode(GPIO.BCM) 
+p3 = 18 # 3rd LED pin #
+GPIO.setup(p3, GPIO.OUT) # Assign pin as output
+
+while True:
+  GPIO.output(p3, 0)
+  sleep(0.5)
+  GPIO.output(p3, 1)
+  sleep(0.5)
