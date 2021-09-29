01ebf17c0ab47fe74750baa5804c1a634c6cc235
vnathan1 <91503462+vnathan1@users.noreply.github.com>
2021-09-29 01:42:23 +0000

    added in button 2

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ i1 = 22 # Button 1
 i2 = 23 # Button 2 
 
 GPIO.setup(p1, GPIO.OUT) # Assign LED 1 output pin
+GPIO.setup(p2, GPIO.OUT) # Assign LED 2 output pin
 GPIO.setup(p3, GPIO.OUT) # Assign LED 3 output pin
 
 # Initial conditions:
 GPIO.output(p1, 0)
-
+GPIO.output(p2, 0)
 GPIO.setup(i1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+GPIO.setup(i2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # LED 1 threaded callback function:
 def myCallback(i1):
@@ -32,9 +33,17 @@ def myCallback(i1):
     sleep(0.5)
     GPIO.output(p1, 0)
     sleep(0.5)
-
+    
+# LED 2 threaded callback function:
+def myCallback2(i2):
+    GPIO.output(p1, 1)
+    sleep(0.5)
+    GPIO.output(p1, 0)
+    sleep(0.5)
 
 GPIO.add_event_detect(i1, GPIO.RISING, callback= myCallback, bouncetime=100)
+
+GPIO.add_event_detect(i2, GPIO.RISING, callback= myCallback2, bouncetime=100)
 
 # LED 3 Constant Blinking:
 try:
